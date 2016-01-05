502fd8476d7ae090776b1b05c9b5b151b8b09f35
mrClapham <info@gcdesign.co.uk>
2016-01-05 13:11:07 +0000

    The listeners are being added and the close button - with force - is working.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# closing_events_1773
-Assesing usecase issue closing OpenFin apps from taskbar.
+# closing events 1773
+Assesing usecase issue, closing OpenFin apps from taskbar.
+
+Open a terminal window and navigate to the 'closing_events_1773' directory.
+
+run 
+
+```
+$ npm start
+```
+This should start a simple Node server, then, click the link below to install.
+
+[installer](https://dl.openfin.co/services/download?fileName=closing_events_1773&config=http://localhost:5070/app.json)
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+var _mainWindow;
+
 document.addEventListener('DOMContentLoaded', function() {
   // body...
   init();
@@ -15,10 +17,50 @@ init = function(){
 
 
 initWithOpenFin = function(){
-console.log("We have OpenFin")
+console.log("We have OpenFin available.");
+    if (!_mainWindow){
+        _mainWindow = fin.desktop.Window.getCurrent()
+    };
+    OpenFinEventListeners.addAllEventListeners(_mainWindow);
+    OpenFinEventListeners.listen("close-requested", closeRequestedCallback);
+    initButtonListeners();
 };
 
+initButtonListeners = function(){
+    document.querySelector("#closeButton").addEventListener('click', function(e){
+        closeMainWindow();
+    });
+
+    document.querySelector('#removeAllListeners').addEventListener("click", function(e){
+        OpenFinEventListeners.removeAllEvemtListeners(_mainWindow);
+    });
+
+    document.querySelector('#removeBoundsChanged').addEventListener("click", function(e){
+        OpenFinEventListeners.removeListenerByName(_mainWindow, "bounds-changed");
+    });
+
+    document.querySelector('#removeError').addEventListener("click", function(e){
+        OpenFinEventListeners.removeListenerByName(_mainWindow, "nonesense");
+    });
+};
+
+closeRequestedCallback = function(evt){
+    console.log("Close Requested Callback ", evt)
+};
+
+closeMainWindow = function(){
+    // OpenFinEventListeners.removeListenerByName(_mainWindow, "close-requested");
+
+    _mainWindow.close(true,
+                        function(){
+                            console.log("Close called ")
+                        },
+                        function(evt){
+                            console.log("Close call failed  ", evt)
+                        }
+                        );
+}
 
 initWithoutOpenFin = function(){
-    console.log("We have NO OpenFin")
+    console.log("We have NO OpenFin ");
 };
